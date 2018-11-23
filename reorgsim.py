57cd92f568d5a4e7f9e06447480535655bed344a
Jonathan Toomim <j@toom.im>
2018-11-22 17:06:54 -0800

    Estimate blocks that need to be mined for PoW parity. Improve formatting.

--- reorgsim.py
+++ reorgsim.py
@@ -17,7 +17,7 @@ class Block:
         return self.name
 
     def __repr__(self):
-        return "%s: p=%s, fs=%f, h=%i, d=%f, pow=%f" % (
+        return "[%6s: parent=%6s, time=%6.0f, height=%3i, diff=%1.2f, PoW=%5.1f]" % (
             self.name, str(self.parent), self.firstseen, self.height, self.difficulty, 
             self.pow)
 
@@ -43,6 +43,7 @@ def time_to_beat(enemytip, pow):
     if not enemytip.parent:
         print "huh, couldn't find an equivalent PoW block"
         raise
+
     interp = (pow - enemytip.parent.pow) / (enemytip.pow - enemytip.parent.pow)
     return enemytip.parent.firstseen + interp * (enemytip.firstseen - enemytip.parent.firstseen)
 
@@ -52,26 +53,31 @@ def compare_blocks_toomim_time(a, b, tc=600.):
         if debug: print "Hey, you're forkless!"
         return compare_blocks_simple_pow(a, b)
     bestchain = None
-    for chaintip in [a, b]:
+    for chaintip, opponent in ((a, b), (b,a)):
         blk = chaintip
+        while blk.pow < opponent.pow:
+            blk = Block(blk, opponent.firstseen, tag='-TBD')
         chainpenalty = 1.
+        blocks = []
         while blk != root:
+            blocks.append(blk)
+            blk = blk.parent
+        for blk in reversed(blocks):
             ts = time_to_beat(a if chaintip == b else b, blk.pow)
             delay = max(0, blk.firstseen - ts)
             penalty = delay / tc
             chainpenalty += penalty / (blk.height - root.height)**.5
-            if debug>2: print "    Block %s increased chain penalty by %f to %f from %f sec delay" % (
+            if debug>2: print "    Block %6s increased chain penalty by %4.2f to %5.2f from %4.0f sec delay" % (
                 str(blk), penalty, chainpenalty, delay)
-            blk = blk.parent
         score = (chaintip.pow - root.pow) / chainpenalty
-        if debug>1: print "Overall for %s: score=%f, pow=%f, penalty=%f" % (
+        if debug>1: print "Overall for %s: score=%f, pow=%f, penalty=%f\n" % (
             str(chaintip), score, chaintip.pow - root.pow, chainpenalty)
         if bestchain == None or score > bestchain[0]:
             bestchain = [score, chaintip]
     return bestchain[1]
 
 if __name__ == "__main__":
-    root = Block(None, 0, '-genesis')
+    root = Block(None, 0, '')
     chain_a = [root]
     t = 0
     for i in range(random.randint(5, 20)):
@@ -84,15 +90,34 @@ if __name__ == "__main__":
     for i in range(random.randint(5, 20)):
         t += random.random() * 600
         chain_b.append(Block(chain_b[-1], t, tag="-B"))
+    del chain_b[0]
     
-    print "\nchain_a:"
-    for block in chain_a: print `block`
-    print "\nchain_b:"
-    for block in chain_b: print `block`
+    # print "Chain A:"
+    # for block in chain_a: print `block`
+    # print "\nChain B:"
+    # for block in chain_b: print `block`
+
+    print "Chain A:\t\t\tChain B:"
+    for h in range(max(chain_a[-1].height, chain_b[-1].height)):
+        a, b = None, None
+        for blk in chain_a:
+            if blk.height == h:
+                a = blk
+                break
+        for blk in chain_b:
+            if blk.height == h:
+                b = blk
+                break
+        print `a` if a else " "*70, `b` if b else ""
 
 
-    print "\ntarget: ", tgt
-    print find_shared_ancestor(chain_a[-1], chain_b[-1])
+
+    #for block in chain_a: print `block`
+    #print "\nChain B:"
+    #for block in chain_b: print `block`
+
+
+    print "\nPenalty and score calculations:"
 
     swinner = compare_blocks_simple_pow(chain_a[-1], chain_b[-1])
     ttwinner = compare_blocks_toomim_time(chain_a[-1], chain_b[-1])
